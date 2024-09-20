96596ad3552235b66412886fb011e1291e6bac16
Sonix <sonitsharma06@gmail.com>
2024-09-20 19:03:47 +0530

    Base Plugin

---
+++ src/main/java/me/sonix/omniTool/OmniTool.java
@@ -0,0 +1,25 @@
+package me.sonix.omniTool;
+
+import me.sonix.omniTool.Commands.giveKitsCommand;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.inventory.InventoryInteractEvent;
+import org.bukkit.event.player.PlayerItemDamageEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerToggleSprintEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public final class OmniTool extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("[OmniTool v1.0 ] -- Starting");
+
+        getCommand("giveKit").setExecutor(new giveKitsCommand());
+    }
+
+
+}
